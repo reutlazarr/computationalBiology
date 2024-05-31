4f8cfd9d8cfd3806da068eb9c88537d6c21770b6
Reut Lazar <reootlazar@gmail.com>
2024-05-31 11:17:06 +0300

    adding zebra index

--- sol.py
+++ sol.py
@@ -20,45 +20,37 @@ class CellularAutomaton:
         self.grid, self.next_grid = self.next_grid, self.grid
 
     def rule(self, x, y):
-            left = self.grid[x, (y-1)%self.size]
-            leftAbove = self.grid[(x-1)%self.size, (y-1)%self.size]
-            leftBelow = self.grid[(x+1)%self.size, (y-1)%self.size]
-            right = self.grid[x, (y+1)%self.size]
-            rightAbove = self.grid[(x-1)%self.size, (y+1)%self.size]
-            rightBelow = self.grid[(x+1)%self.size, (y+1)%self.size]
-            above = self.grid[(x-1)%self.size, y]
-            below = self.grid[(x+1)%self.size, y]
-            threshold=0
-            me= self.grid[x][y]
-            if me == above:
-                threshold += 1/8
-            if me == below:
-                threshold += 1/8  
-            if me != left:
-                threshold += 1/8  
-            if me != right:
-                threshold += 1/8 
-            if me != rightAbove:
-                threshold += 1/8
-            if me != rightBelow:
-                threshold += 1/8 
-            if me != leftAbove:
-                threshold += 1/8
-            if me != leftBelow:
-                threshold += 1/8
-            if threshold==0.5  :
-                random_number= random.random()
-                if random_number >0.5:
-                    return 1
-                else :
-                    return 0
-
-            if 0.5 > threshold:
-                return 1-me
-            return me
-
-
-  
+        left = self.grid[x, (y-1) % self.size]
+        leftAbove = self.grid[(x-1) % self.size, (y-1) % self.size]
+        leftBelow = self.grid[(x+1) % self.size, (y-1) % self.size]
+        right = self.grid[x, (y+1) % self.size]
+        rightAbove = self.grid[(x-1) % self.size, (y+1) % self.size]
+        rightBelow = self.grid[(x+1) % self.size, (y+1) % self.size]
+        above = self.grid[(x-1) % self.size, y]
+        below = self.grid[(x+1) % self.size, y]
+        threshold = 0
+        me = self.grid[x][y]
+        if me == above:
+            threshold += 1/8
+        if me == below:
+            threshold += 1/8  
+        if me != left:
+            threshold += 1/8  
+        if me != right:
+            threshold += 1/8 
+        if me != rightAbove:
+            threshold += 1/8
+        if me != rightBelow:
+            threshold += 1/8 
+        if me != leftAbove:
+            threshold += 1/8
+        if me != leftBelow:
+            threshold += 1/8
+        if threshold == 0.5:
+            return random.choice([0, 1])
+        if threshold < 0.5:
+            return 1 - me
+        return me
 
     def stripe_index(self):
         mismatches = 0
@@ -67,6 +59,27 @@ class CellularAutomaton:
                 if self.grid[i, j] == self.grid[i + 1, j]:
                     mismatches += 1
         return mismatches / (self.size * (self.size - 1))
+
+    def zebra_index(self):
+        score = 0
+        for j in range(self.size):
+            white_ratio = np.sum(self.grid[:, j]) / self.size  # Ratio of white cells in the column
+            dominant_color = 1 if white_ratio > 0.5 else 0  # 1 if most cells are white, 0 if most are black
+
+            # Neighboring columns
+            left_col = self.grid[:, (j-1) % self.size]
+            right_col = self.grid[:, (j+1) % self.size]
+
+            left_ratio = np.sum(left_col) / self.size  # Ratio of white cells in the left column
+            right_ratio = np.sum(right_col) / self.size  # Ratio of white cells in the right column
+
+            left_color = 1 if left_ratio > 0.5 else 0
+            right_color = 1 if right_ratio > 0.5 else 0
+
+            if left_color != dominant_color and right_color != dominant_color:
+                score += white_ratio if dominant_color == 1 else (1 - white_ratio)
+
+        return score / self.size  # Normalize by the size of the matrix
 
 # Create the main window
 root = tk.Tk()
@@ -79,6 +92,7 @@ canvas.pack()
 # Initialize the cellular automaton
 ca = CellularAutomaton(GRID_SIZE)
 stripe_indices = []
+zebra_indices = []
 generation = 0
 
 def draw_grid():
@@ -93,17 +107,16 @@ def draw_grid():
             canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
             
     draw_generation_number()
-
     draw_zebra_head()
     draw_zebra_legs()
     draw_zebra_tail()
 
 def draw_zebra_head():
     # Head is a simple oval
-    head_x1 = (GRID_SIZE  - 5) * CELL_SIZE +100
+    head_x1 = (GRID_SIZE  - 5) * CELL_SIZE + 100
     head_y1 = 10
-    head_x2 = (GRID_SIZE + 5) * CELL_SIZE +100
-    head_y2 = 10 * CELL_SIZE +10
+    head_x2 = (GRID_SIZE + 5) * CELL_SIZE + 100
+    head_y2 = 10 * CELL_SIZE + 10
     canvas.create_oval(head_x1, head_y1, head_x2, head_y2, fill="white", outline="black")
 
     # Eyes
@@ -126,9 +139,9 @@ def draw_zebra_legs():
     leg_height = 5 * CELL_SIZE
     leg_positions = [
         (GRID_SIZE // 4, GRID_SIZE + 11),
-        (GRID_SIZE // 4 + 15, GRID_SIZE + 11  ),
-        (3 * GRID_SIZE // 4 - 15, GRID_SIZE + 11  ),
-        (3 * GRID_SIZE // 4, GRID_SIZE +11)
+        (GRID_SIZE // 4 + 15, GRID_SIZE + 11),
+        (3 * GRID_SIZE // 4 - 15, GRID_SIZE + 11),
+        (3 * GRID_SIZE // 4, GRID_SIZE + 11)
     ]
     for pos in leg_positions:
         x1 = pos[0] * CELL_SIZE
@@ -140,31 +153,32 @@ def draw_zebra_legs():
 def draw_zebra_tail():
     tail_x1 = 25
     tail_y1 = (GRID_SIZE // 2) * CELL_SIZE
-    tail_x2 =  10 * CELL_SIZE +25
+    tail_x2 = 10 * CELL_SIZE + 25
     tail_y2 = tail_y1 + 1 * CELL_SIZE
     canvas.create_line(tail_x1, tail_y1, tail_x2, tail_y2, fill="black", width=5)
 
+def plot_zebra_index():
+    plt.plot(zebra_indices, color='tab:red')
+    plt.xlabel('Generation')
+    plt.ylabel('Zebra Index')
+    plt.title('Zebra Index Over Generations')
+    plt.ylim(0, 1)  # Ensure the y-axis ranges from 0 to 1
+    plt.show()
+
 def update():
     global generation
-    if generation < 1000:
+    if generation < 350:
         ca.update()
         draw_grid()
-        stripe_indices.append(ca.stripe_index())
+        zebra_indices.append(ca.zebra_index())
         generation += 1
         root.after(100, update)
     else:
-        plot_stripe_indices()
+        plot_zebra_index()
 
 def draw_generation_number():
     # Display the current generation number
     canvas.create_text(50, 20, text=f"Generation: {generation}", anchor="nw", font=("Helvetica", 16))
-
-def plot_stripe_indices():
-    plt.plot(stripe_indices)
-    plt.xlabel('Generation')
-    plt.ylabel('Stripe Index')
-    plt.title('Stripe Index Over Generations')
-    plt.show()
 
 # Draw the initial grid
 draw_grid()
