1b89b79aa5d7ceb5526ad6a49620c646f2ec35f7
nshakedsl <shakedsolomon@gmail.com>
2024-06-25 21:18:34 +0300

    eletisim not work

--- ex2/sol.py
+++ ex2/sol.py
@@ -35,23 +35,26 @@ def select_parents(population, fitness_scores):
     return parents
 
 def crossover(parent1, parent2):
-    point = random.randint(1, len(parent1) - 1)
+    size = len(parent1)
+    point = random.randint(1, size - 1)
     child = parent1[:point] + parent2[point:]
+    seen_women = set()
+    seen_men = set()
+   
+    for man, woman in child[:point]:
+        seen_women.add(woman)
+        seen_men.add(man)
     
-    seenWoman = set()
-    seenMan = set()
-    for man, woman in child:
-        if woman in seenWoman:
-            available_women = set(range(1, n+1)) - seenWoman
-            woman = random.choice(list(available_women))
-        seenWoman.add(woman)
-        
-        if man in seenMan:
-            available_men = set(range(1, n+1)) - seenMan
-            man = random.choice(list(available_men))
-        seenMan.add(man)
-    
-    return [[man, woman] for man, woman in zip(seenMan, seenWoman)]
+    for i, [man, woman] in enumerate(child[point:]):
+        if man in seen_men :
+            child[i+point][0] = random.choice(list(set(range(1, size + 1))-seen_men))  
+
+            seen_men.add(child[i+point][0])
+        if woman in seen_women:
+            child[i+point][1] = random.choice(list(set(range(1, size + 1))-seen_women))
+            seen_women.add(child[i+point][1])
+        return child
+
 
 
 def mutate(solution, mutation_rate=0.05):
@@ -66,16 +69,23 @@ def genetic_algorithm(men_preferences, women_preferences, num_generations=100, p
     for _ in range(num_generations):
         new_population = []
         fitness_scores = [calculate_fitness(solution, men_preferences, women_preferences) for solution in population]
-        print('fitness_scores: ', fitness_scores[0])
+        print('fitness_scores: ', sorted(fitness_scores, reverse=True)[:1])
+        # elitism
+        best_solutions = sorted(population, key=lambda x: fitness_scores[population.index(x)], reverse=True)[:int(population_size*0.05)]
+        new_population.extend(best_solutions)
+       
+
+        # print('len!!! ', len(best_solutions))
+        # for i in range(5):
+            # print('best_solutions: ', best_solutions[i])
+            # print('best_solutions: ', calculate_fitness(best_solutions[i],men_preferences, women_preferences))
+      
         for _ in range(population_size-int(population_size*0.05)):
             parent1, parent2 = select_parents(population,fitness_scores)
             child1 = crossover(parent1, parent2)
             child1 = mutate(child1)
             new_population.extend([child1])
-        # elitism
-        best_solutions = sorted(population, key=lambda x: fitness_scores[population.index(x)])[:int(population_size*0.05)]
         # print('best_solutions: ', best_solutions[0])
-        new_population.extend(best_solutions)
 
         population = new_population
  
