cc8abf4665b9468587417b9a64385540ce764c43
nshakedsl <shakedsolomon@gmail.com>
2024-07-14 13:36:56 +0300

    ex3

--- ex3/sol.py
+++ ex3/sol.py
@@ -11,12 +11,12 @@ digits_keys = pd.read_csv(keys_path, header=None).values.flatten()
 # Parameters
 num_neurons = 10  # Number of neurons in each dimension (10x10 grid)
 input_len = 784  # Length of input vector (28x28 images)
-sigma = 1.0  # Neighborhood radius
+raduis = 1.0  # Neighborhood radius
 learning_rate = 0.5  # Learning rate
 num_iterations = 10000  # Number of iterations
 
 # Initialize the weights
-weights = np.random.rand(num_neurons, num_neurons, input_len)
+weights = np.random.rand(num_neurons, num_neurons, input_len) * 255
 
 def euclidean_distance(x, y):
     return np.sqrt(np.sum((x - y) ** 2))
@@ -24,35 +24,34 @@ def euclidean_distance(x, y):
 def decay_function(initial, t, max_iter):
     return initial * np.exp(-t / max_iter)
 
-def train_som(data, weights, num_iterations, sigma, learning_rate):
+def train_som(data, weights, num_iterations, raduis, learning_rate):
     for t in range(num_iterations):
-        # Randomly select a sample from the data
-        sample = data[np.random.randint(0, data.shape[0])]
+        for vector in data:
+            # Find the Best Matching Unit (BMU)
+            bmu_idx = np.array([0, 0])
+            min_dist = np.inf
+            for i in range(num_neurons):
+                for j in range(num_neurons):
+                    dist = euclidean_distance(vector, weights[i, j])
+                    if dist < min_dist:
+                        min_dist = dist
+                        bmu_idx = np.array([i, j])
+            
+            # Update the weights
+            for i in range(num_neurons):
+                for j in range(num_neurons):
+                    neuron_pos = np.array([i, j])
+                    dist_to_bmu = euclidean_distance(neuron_pos, bmu_idx)
+                    if dist_to_bmu <= raduis:
+                        Neighborhood = np.exp(-dist_to_bmu ** 2 / (2 * raduis ** 2))
+                        current_error = vector - weights[i, j]
+                        weights[i, j] += Neighborhood * learning_rate * current_error
         
-        # Find the Best Matching Unit (BMU)
-        bmu_idx = np.array([0, 0])
-        min_dist = np.inf
-        for i in range(num_neurons):
-            for j in range(num_neurons):
-                dist = euclidean_distance(sample, weights[i, j])
-                if dist < min_dist:
-                    min_dist = dist
-                    bmu_idx = np.array([i, j])
-        
-        # Update the weights
-        for i in range(num_neurons):
-            for j in range(num_neurons):
-                neuron_pos = np.array([i, j])
-                dist_to_bmu = euclidean_distance(neuron_pos, bmu_idx)
-                if dist_to_bmu <= sigma:
-                    influence = np.exp(-dist_to_bmu ** 2 / (2 * sigma ** 2))
-                    weights[i, j] += influence * learning_rate * (sample - weights[i, j])
-        
-        # Decay sigma and learning rate
-        sigma = decay_function(sigma, t, num_iterations)
+        # Decay raduis and learning rate
+        raduis = decay_function(raduis, t, num_iterations)
         learning_rate = decay_function(learning_rate, t, num_iterations)
 
-train_som(data, weights, num_iterations, sigma, learning_rate)
+train_som(data, weights, num_iterations, raduis, learning_rate)
 
 def plot_som_with_labels(weights, data, labels):
     plt.figure(figsize=(10, 10))
